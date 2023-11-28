ea2ffb478757ee8e3197230973a2f69986e0de0d
Suman Ghosh <ghoshs91439@gmail.com>
2023-11-28 19:15:17 +0530

    fixed git problem

--- src/App.js
+++ src/App.js
@@ -4,28 +4,28 @@ import Header from './component/header/header';
 import Tab from './component/tabs/tab';
 import Delivery from './Delivery/delivery';
 import Diningout from './Dining out/diningout';
-import Nightlife from './NightLife/nightlife'
+import Nightlife from './NightLife/nightlife';
 import Subfooter from './component/Footer/subfooter';
 import Footer from './component/Footer/footer';
+import ItemList from './component/itemlist/itemContainer';
+import DefaultPage from './component/defaultpage/default';
 import './App.css';
 
 function App() {
   return (
-    <div className="App mx-60  max-2xl:mx-7 max-md:mx-1 font-sans">
+    <div className="App mx-60 max-2xl:mx-7 max-md:mx-1 font-sans">
       <Router>
         <Header />
         <Tab />
         <Routes>
           <Route path="/" element={<Delivery />} />
-        </Routes>
-        <Routes>
           <Route path="/diningout" element={<Diningout />} />
-        </Routes>
-        <Routes>
           <Route path="/nightlife" element={<Nightlife />} />
+          <Route path="/itemlist/:name/:img" element={<ItemList />} />
+          <Route path="*" element={<DefaultPage />} />
         </Routes>
-        <Subfooter/>
-        <Footer/>
+        <Subfooter />
+        <Footer />
       </Router>
     </div>
   );
